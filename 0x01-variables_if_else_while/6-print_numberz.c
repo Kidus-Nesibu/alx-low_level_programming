b9dbba3b5e04531a5d04f3f2457f3a7dfc022ae4
Kidus-Neibu <knesibu3@gmail.com>
2022-12-12 13:31:16 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number < 10; number++;) {
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
