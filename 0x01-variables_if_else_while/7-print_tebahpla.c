cf267fa1e1b0b28c305644b61a552074f9b66a6b
Kidus-Neibu <knesibu3@gmail.com>
2022-12-12 14:06:40 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always
+ */
+int main(void)
+{
+	char reverse = 'z';
+
+	while (reverse > 'z')
+	{
+		putchar(reverse);
+		reverse--;
+	}
+	return (0);
+}
