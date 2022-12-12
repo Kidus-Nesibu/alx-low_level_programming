0f88f9f9a1a1babc053ecaab83232d2ae3881aad
Kidus-Neibu <knesibu3@gmail.com>
2022-12-12 14:30:35 -0800

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number = 0;
+	char alphabet = 'a';
+
+	while (number < 10)
+	{
+		putchar(number);
+		number++;
+		while (alphabet < 'g')
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+		putchar('\n');
+	}
+	return (0);
+
+}
