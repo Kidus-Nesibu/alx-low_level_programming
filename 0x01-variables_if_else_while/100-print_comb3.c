a124ac1f813043a22a2e53f61334409ddc5f1076
Kidus-Neibu <knesibu3@gmail.com>
2022-12-12 16:34:50 -0800

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,15 @@ int main(void)
 
 	for (tens = 0; tens <= 9; tens++)
 	{
-		putchar(tens + '0');
-		putchar(ones + '0');
-		if (tens < 8)
+		for (ones = tens + 1; ones <= 9; ones++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
