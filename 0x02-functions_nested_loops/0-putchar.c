56e9e2755a04e75b77c70b3958bb872311008370
Kidus-Neibu <knesibu3@gmail.com>
2022-12-14 10:19:41 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
-
-
+/**
+ * main - Displays _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 /**
  * main - dont understand it
- * return:always 0
  */
 
 int _putchar(char);
@@ -15,3 +14,5 @@ int print_last_digit(int);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+
