6182d8571cee4deb684bea7c944a74d2b02ec951
Kidus-Neibu <knesibu3@gmail.com>
2022-12-14 12:10:32 -0800

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-/**
- * main - dont understand it
- */
-
 int _putchar(char);
-void print_alphabet(void);
-void print_alphabet(void);
-int _islower(int c);
-int _isalpha(int c);
-int _isalpha(int c);
-int _abs(int);
-int print_last_digit(int);
-int print_last_digit(int);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-
-
