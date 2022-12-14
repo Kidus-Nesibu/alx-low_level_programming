fb05193c10adb294fbc9846272b3f1c0e9444593
Kidus-Neibu <knesibu3@gmail.com>
2022-12-14 08:39:34 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - displays _putchar
+ * main - Displays _putchar
  * Return: Always 0
  */
 int main(void)
@@ -17,4 +17,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
