b4db4af981721c5f5d894066d4f3840debc26c00
Kidus-Nesibu <knesibu3@gmail.com>
2022-12-20 15:32:12 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reserv string
+ * @s: parameter
+ */
+void rev _string(char *s)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length] != '\0')
+		length++;
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] =  s[length2];
+		s[length2] = tmp;
+		length -= 1;
+	}
+}
