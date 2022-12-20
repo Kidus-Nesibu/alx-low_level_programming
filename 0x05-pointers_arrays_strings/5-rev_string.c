74bd6c00f058d9f991193ffa016f4cdf8bb19ce0
Kidus-Nesibu <knesibu3@gmail.com>
2022-12-20 15:37:55 -0800

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,13 +12,13 @@ void rev _string(char *s)
 	length2 = 0;
 
 	while (s[length1] != '\0')
-		length++;
+		length1++;
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
 		s[i] =  s[length2];
 		s[length2] = tmp;
-		length -= 1;
+		length2 -= 1;
 	}
 }
