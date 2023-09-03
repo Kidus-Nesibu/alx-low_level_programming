4ab5f779d823b80ff447dd94ddbb7d73aac15af5
Kidus-Nesibu <knesibu3@gmail.com>
2023-09-03 13:52:33 -0700

    fixed betty style documentation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-/* main - prints the argument name
+/**
+ * main - prints the argument name
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 if success  
+ * Return: 0 if success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s", argv[0]);
 	printf("\n");
-	return 0;
+	return (0);
 }
