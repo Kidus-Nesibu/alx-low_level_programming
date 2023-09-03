69b7e3947e7749f10533e536ad6420e194a9e6c6
Kidus-Nesibu <knesibu3@gmail.com>
2023-09-03 13:44:55 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/* main - prints the argument name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success  
+ */
 int main (int argc, char *argv[])
 {
 	printf("%s", argv[0]);
