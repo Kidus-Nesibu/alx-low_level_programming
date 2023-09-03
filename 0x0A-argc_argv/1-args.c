781acd28c5cdef3b1604657a39b54990a71a196e
Kidus-Nesibu <knesibu3@gmail.com>
2023-09-03 14:15:39 -0700

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - prints the number of argument
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
