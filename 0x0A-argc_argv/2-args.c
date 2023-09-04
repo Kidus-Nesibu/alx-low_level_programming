5e6efdf2bb4ed9aae429adeb358c0957c251517d
Kidus-Nesibu <knesibu3@gmail.com>
2023-09-04 11:38:57 -0700

    task no 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - displays the argument 
+ * @argc: argument count
+ * @argv: arguent value 
+ * Return: 0 if sucsses
+ */
+int main (int argc char *argv[])
+{
+	int i;
+	printf("%s", *argv[0]);
+	for(i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);	
+	}
+	return (0);
+}
