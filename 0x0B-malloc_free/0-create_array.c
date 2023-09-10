c0e7758e53b5cfbe7734fd2b3f7fd1fef2a99c96
Kidus-Nesibu <knesibu3@gmail.com>
2023-09-10 14:50:52 -0700

    fixed betty issues

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,17 +1,29 @@
 #include<stdlib.h>
 #include"main.h"
+/**
+ * create_array - create array of character
+ * @size: unsigned integer
+ * @c: character variable
+ * Return: character pointer
+ */
 char *create_array(unsigned int size, char c)
 {
+	int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = malloc(sizeof(char) *size);
-	int i;
-	
-	for (i=0;i<size;i++)
+	char *array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
 	{
 		*array = c;
+
 	}
 	return (array);
 }
