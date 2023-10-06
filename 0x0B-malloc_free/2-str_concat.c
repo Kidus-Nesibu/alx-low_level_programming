38bf4fa1597753220638d344a39087d1d265bd87
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-06 04:39:19 -0700

    hot fix again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *p;
 
-//	if (s1 == NULL || s2 == NULL)
+/**	if (s1 == NULL || s2 == NULL)
 //	{
 //		return ("");
 //	}
+*/
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
