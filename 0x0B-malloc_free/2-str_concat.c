9132f8cea37afd479b1aa1291c40d9bf57ca5dd7
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-06 06:32:08 -0700

    hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,47 @@
 #include"main.h"
 #include<stdlib.h>
 /**
- * str_concat - concatnates
- * @s1:	 parameter 1
+ * str_concat - concat strings
+ * @s1: parameter 1
  * @s2: parameter 2
- * Return: base address dyamic memo
+ * Return: pointer to dynamic memo
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1;
-	int len2;
-	int i;
+	int i, len1, len2;
 	char *p;
 
-/**	if (s1 == NULL || s2 == NULL)
-//	{
-//		return ("");
-//	}
-*/
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-	len2++;
-	p = malloc(sizeof(char) * (len1 + len2));
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else
+	{
+		for (len1 = 0; s1[len1]; len1++)
+			;
+	}
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		for (len2 = 0; s2[len2]; len2++)
+			;
+	}
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len1; i++)
+	for (i = 0; i<len1; i++)
 	{
 		p[i] = s1[i];
 	}
-	for (i = 0; i < len2; i++)
+	for (i = 0; i<len2; i++)
 	{
 		p[len1 + i] = s2[i];
 	}
-	p[len1 + len2 - 1] = '\0';
+	p[len1 + len2] = '\0';
 	return (p);
 }
