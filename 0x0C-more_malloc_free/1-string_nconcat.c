2f34fe0afd64bceb4512318a0fd5715408ae1a93
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-07 17:36:00 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,43 +5,44 @@
  * @s1: string 1
  * @s2: string 2
  * @n: number of string to be copied
- * Retrun: pointer
+ * Return: pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int i, len1, len2;
 	char *p;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			;
+	}
+	else
+	{
+		len1 = 0;
+	}
+	if (s2 != NULL)
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+			;
+	}
+	else
+	{
+		len2 = 0;
+	}
 	p = malloc(sizeof(char) * (len1 + (n >= len2 ? len2 : n) + 1));
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len2; i++)
 	{
 		p[i] = s1[i];
 	}
-	if (n >= len2)
+	for (i = 0; i < (n >= len2 ? len2 : n); i++)
 	{
-		for (i = 0; i < len2; i++)
-		{
-			p[len1 + i] = s2[i];
-		}
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			p[len1 + i] = s2[i];
-		}
+		p[len1 + i] = s2[i];
 	}
 	p[len1 + i] = '\0';
 	return (p);
