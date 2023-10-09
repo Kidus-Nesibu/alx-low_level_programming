95c164b86d701cbf17d81efb3ee07132f1244bca
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-09 16:56:53 -0700

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,19 +9,29 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL || d->owner == NULL)
-		{
-			printf("nil\n");
-		}
-		else if (d->age == -1)
+		if (d->name == NULL)
 		{
 			printf("nil\n");
 		}
 		else
 		{
-			printf("Name: %s\n", (*d).name);
-			printf("Age: %f\n", (*d).age);
-			printf("Owner: %s\n", (*d).owner);
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == 0)
+		{
+			printf("nil\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+		if (d->owner)
+		{
+			printf("nil\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
