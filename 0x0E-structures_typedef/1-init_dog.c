6af2141756c4c9e77a6b897ff57a1bc7236fd118
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-09 09:02:23 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include"dog.h"
+#include<stdlib.h>
+/**
+ * init_dog-initialize sturct
+ * @name: parameter 1
+ * @age: parameter 2
+ * @owner: paramter 3
+ * @d: pointer parmeter
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	(*d).name = name;
+	(*d).owner = owner;
+	(*d).age = age;
+}
