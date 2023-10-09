e4316648f9ea39c3d79368f65753b4be7549214d
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-09 15:25:16 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,6 +10,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
 	(*d).name = name;
 	(*d).owner = owner;
 	(*d).age = age;
