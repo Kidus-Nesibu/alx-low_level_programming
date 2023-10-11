acfa09707721498b34a39b0ce4c56116822524b5
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-11 10:41:52 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - start of a prog
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 when success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
