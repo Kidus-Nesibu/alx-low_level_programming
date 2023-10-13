8b6238c07264b63f1a5d10c1add26b022e12353d
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-13 03:05:35 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL || action != NULL || size != 0)
+	if (array != NULL && action != NULL || size != 0)
 	{
 		unsigned int i;
 
