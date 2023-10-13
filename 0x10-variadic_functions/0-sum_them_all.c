5e06c3d45c9a2ad4b81f5f457029ac7f78a91795
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-13 05:56:28 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include"variadic_functions.h"
 /**
+ * sum_them_all - add the arguments
  * @n: number of arguments
  * Return: sum of arguments or 0
  */
@@ -24,14 +25,3 @@ int sum_them_all(const unsigned int n, ...)
 		return (sum);
 	}
 }
-int main(void)
-{
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);
-    return (0);
-}
-
