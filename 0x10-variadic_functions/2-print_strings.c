9dfb305a5f16ef2a938bc7c9c61df23f50cdadd5
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-13 07:32:37 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include"variadic_functions.h"
+/**
+ * print_strings - display strings
+ * @separator: comma
+ * @n: arguments
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+		{
+			printf("nil");
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("%s", str);
+			if (i < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	printf("\n");
+}
