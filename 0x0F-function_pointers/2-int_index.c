7f345313d0fc58dd8f2ccbb91bbe989b2d1fc51a
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-13 13:53:35 -0700

    tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,34 @@
-#include"function_pointers.h"
+#include <stdio.h>
+
 /**
- * int_index - searchs for integer
- * @size: array length
- * @cmp: function pointer
- * @array: elements
- * Return: value based on the conditions
+ * int_index - searchs for an integer
+ *
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to a function
+ *
+ * Return: -1 -> if no element match, or size <= 0
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL && cmp == NULL)
+	int i, counter;
+
+	counter = 0;
+	if (array != NULL && cmp != NULL)
 	{
-		unsigned int i;
-
-		if (size <= 0)
-		{
-			return (-1);
-		}
-
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 			{
+				counter++;
 				return (i);
 			}
 		}
-		return (-1);
 	}
+	if (size <= 0 || counter == 0)
+		return (-1);
+	else
+		return (0);
 }
+
