1b5070d3afae88ae7b9959b7878b9e168816edf3
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-22 12:42:19 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include"lists.h"
+/**
+ * print_list - count and print nodes
+ * @h: struct header
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+		}
+		else
+		{
+			printf("[%u]%s\n", h->len, h->str);
+			count++;
+		}
+		h = h->next;
+	}
+	return (count);
+}
