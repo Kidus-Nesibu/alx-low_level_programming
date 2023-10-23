4cebc8f31f669b89a4502eb1200550d76b3a2782
Kidus-Nesibu <knesibu3@gmail.com>
2023-10-23 01:56:54 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@ size_t print_list(const list_t *h)
 	unsigned int count = 0;
 	const list_t *hold;
 
-	while (h != NULL)
+	hold = h;
+	while (hold != NULL)
 	{
-		hold = h;
 
 		if (hold->str == NULL)
 		{
