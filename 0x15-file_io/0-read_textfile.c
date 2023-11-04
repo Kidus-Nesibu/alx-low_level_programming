75a15555096e6b45f9f90fedb83d945a9f579265
Kidus-Nesibu <knesibu3@gmail.com>
2023-11-04 04:14:30 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,37 @@
-#include "main.h"
-
-/**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- *
- * @filename: file name to read
- * @letters: number of letters to read
- *
- * Return: actual number of letters it could read and print
- */
-
+#include"main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t fd_r, fd_w;
-	char *buf;
+	ssize_t fdr, fdw;
+	char *buffer;
 
 	if (filename == NULL)
+	{
 		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	}
+	buffer = malloc(sizeof(char)*letters);
+	if (buffer == NULL)
 	{
 		close(fd);
-		return (0);
+		return (-1);
 	}
-	fd_r = read(fd, buf, letters);
+	fdr = read(fd, buffer, letters);
 	close(fd);
-	if (fd_r == -1)
+	if (fdr == -1)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	fd_w = write(STDOUT_FILENO, buf, fd_r);
-	free(buf);
-	if (fd_r != fd_w)
+	fdw = write(STDOUT_FILENO, buffer, fdr);
+	free(buffer);
+	if (fdr != fdw)
+	{
 		return (0);
-
-	return (fd_w);
+	}
+	return (fdw);
 }
