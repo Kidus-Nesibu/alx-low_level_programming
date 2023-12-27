758468cd501c3380a4aaa93b6d2fb01005eea741
Kidus-Nesibu <knesibu3@gamil.com>
2023-12-27 11:18:04 +0300

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,20 @@
+/**
+ * main - prints alphabet in lower case except q and e
+ * Return: 0
+*/
 #include<stdio.h>
-int main ()
+int main(void)
 {
 	int c;
-	for(c='a';c<='z';c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c=='q'||c=='e')
+		if (c == 'q' || c == 'e')
 		{
 			continue;
 		}
 		putchar(c);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
