3d8f3f2ade2a56cec3451329c88a6a28d367e5f2
Kidus-Nesibu <knesibu3@gamil.com>
2024-01-17 20:23:43 +0300

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include"dog.h"
+/**
+ * init_dog - intializes struct dog
+ * @d: struct pointer
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog my_dog;
+
+	dog.name = "poppy";
+	dog.age = 3.5;
+	dog.owner = "Bob";
+}
