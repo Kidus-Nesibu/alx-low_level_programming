66186e6a98ffc6bb25e0ece1dedfb18834eb92a7
Kidus-Nesibu <knesibu3@gamil.com>
2024-01-18 17:52:15 +0300

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = "poppy";
-		d->age = 3.5;
-		d->owner = "Bob";
+		(*d).name = "poppy";
+		(*d).age = 3.5;
+		(*d).owner = "Bob";
 	}
 }
