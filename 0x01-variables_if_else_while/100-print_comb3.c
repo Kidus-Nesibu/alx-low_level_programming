e6705cc0cf6b3db8b251b46f4bb4fa37a9eb2962
Kidus-Nesibu <knesibu3@gamil.com>
2024-01-25 01:53:14 +0300

    task task 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			if (i != j && i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (j != 9 || i != 9)
+				if (j != 9 || i != 8)
 				{
 					putchar(',');
 					putchar(' ');
