78a7dd0e3554564bed8563a4a6fb19b851c0d4e1
Kidus-Nesibu <knesibu3@gamil.com>
2024-01-25 19:35:49 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+
 /**
  * main - checks the postitibe_or_negative function
  * Return: 0 if the program runs succesfully
  */
+
 int main(void)
 {
-	int n = 0;
+	int i;
 
-	postitibe_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
