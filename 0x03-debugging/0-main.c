754a5cac5a10d80dbde7b2c16c8dc85da9381b10
Kidus-Nesibu <knesibu3@gamil.com>
2024-01-25 19:46:47 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * main - checks the postitibe_or_negative function
- * Return: 0 if the program runs succesfully
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
