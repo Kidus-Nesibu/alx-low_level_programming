8436dff64c960c1b28138c0c3269ba1e69633c0d
Kidus-Nesibu <knesibu3@gmail.com>
2024-01-26 15:25:34 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints _putchar to the standard out put
+ * Return: 0 if the program runs succesfully
+ **/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
