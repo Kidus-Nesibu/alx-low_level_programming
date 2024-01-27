aa641d03cf04d0ab26dfdadd0d197a9b182b8f9a
Kidus-Nesibu <knesibu3@gmail.com>
2024-01-26 17:31:08 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,11 @@
 #include"main.h"
+/**
+ * main - returns the last digit of the given parameter
+ * Return: last digit of the number
+ **/
 int print_last_digit(int n)
 {
 	n = n % 10;
 
-	return(n);
+	return (n);
 }	
