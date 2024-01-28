3e101cba8d093696e975d3faa6631b81b1ec6852
Kidus-Nesibu <knesibu3@gmail.com>
2024-01-28 03:09:38 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for(i = n; n >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		_putchar(10);
+	}
+	for (i = n; n <= 98; i++)
+	{
+		if (i < 0)
+		{
+			printf("-%d, ", i);
+		}
+		else
+		{
+			printf("%d, ", i);
+		}
+	}
+	_putchar(10);
+}
