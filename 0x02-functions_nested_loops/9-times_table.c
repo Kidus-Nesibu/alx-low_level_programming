8bb5be46544bb0084b242923568fe8b8a8ec8e67
Kidus-Nesibu <knesibu3@gmail.com>
2024-01-27 16:26:44 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 
 				_putchar(first + '0');
 				_putchar(last + '0');
-				if (n != 81 && j < 9)
+				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -25,7 +25,7 @@ void times_table(void)
 			else
 			{
 				_putchar(n + '0');
-				if (n != 81 && j < 9)
+				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
