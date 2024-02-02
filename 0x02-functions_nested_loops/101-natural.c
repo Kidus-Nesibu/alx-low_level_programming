6b3ccb74b6d3d4176f521e66152d774d59261975
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-02 00:20:00 -0800

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+int main(void)
+{
+	int i, sum = 0;
+
+	while (i < 1024)
+	{
+
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
