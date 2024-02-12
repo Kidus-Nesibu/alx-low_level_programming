96c52ec888465e0703bf5fda0b64ce74c2e32b80
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-12 09:13:55 -0800

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,9 @@
 #include"main.h"
+/**
+ * _isdigit - check if c is from 0 to 9
+ * @c: the argument to be checked
+ * Return: 1 it the numetr is 0 or 9 0 if it is other number
+ */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 51)
