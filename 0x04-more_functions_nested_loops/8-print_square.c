327b82d3bd7f36d6b623e56b31ec584bd6e2bae6
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-14 08:38:34 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,12 @@
 #include"main.h"
+/**
+ * print_square - prints square
+ * @size: tell us the size of the square
+ */
 void print_square(int size)
 {
 	int i = 1;
+
 	if (size <= 0)
 	{
 		_putchar(10);
@@ -9,6 +14,7 @@ void print_square(int size)
 	while (i <= size)
 	{
 		int j = 1;
+
 		while (j <= size)
 		{
 			_putchar('#');
