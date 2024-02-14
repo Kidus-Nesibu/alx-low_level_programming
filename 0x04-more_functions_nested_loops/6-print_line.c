0b55884f5b9366949020d494ed46dbbf22c0318a
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-14 05:40:33 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,12 @@
 #include"main.h"
+/**
+ * print_line - prints line
+ * @n: determines the length of the line tobe printed
+ */
 void print_line(int n)
 {
 	int i = 1;
-	
+
 	if (n < 0 || n == 0)
 	{
 		_putchar(10);
