cc3f69433d2cbb2fb200f60b2a76aa5aabb88536
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-13 23:46:51 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include"main.h"
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i != 11)
+	{
+		int k = 0;
+
+		while (k <= 1)
+		{
+			int j = 0;
+
+			while (j <= 9)
+			{
+				if ( k == 1)
+				{
+					_putchar(k + '0');
+				}
+				_putchar(j + '0');
+				if (j == 4 && k == 1)
+				{
+					break;
+				}
+				j++;
+			}
+			k++;
+		}
+		_putchar(10);
+		i++;
+	}
+}
+
