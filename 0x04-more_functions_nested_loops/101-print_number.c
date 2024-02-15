10af9d0e88d82681c0f848f6e40389db69e912f7
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-15 07:59:38 -0800

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,20 +9,20 @@ void print_number(int n)
 
 	if (n > 9 && n < 99)
 	{
-		_putchar((n / 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
 		_putchar((n % 10) + '0');
 	}
 	else if (n > 99 && n < 999)
 	{
 		_putchar((n / 100) + '0');
-		_putchar((n / 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
 		_putchar((n % 10) + '0');
 	}
-	else (n > 999)
+	else if (n > 999)
 	{
 		_putchar((n / 1000) + '0');
-		_putchar((n / 100) + '0');
-		_putchar((n / 10) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
 		_putchar((n % 10) + '0');
 	}
 }
