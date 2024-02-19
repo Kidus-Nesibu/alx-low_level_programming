99b45f5aaa620cd9feab342afd1142ce7803d773
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-19 08:37:51 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,13 @@
 /**
  * _strlen - counts the string length
  * @s: the string provide
+ * Return: the lengthe of the stirng s
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		i++;
 		s++;
