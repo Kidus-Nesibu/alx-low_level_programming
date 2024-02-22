35453ffafd2f42be77b148594e3f973120661378
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-22 06:39:54 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,10 @@
 #include"main.h"
 void puts2(char *str)
 {
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		if (i % 2 != 0)
-		{
-			continue;
-		}
-
-		_putchar(str[i]);
-		i++;
-		
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar(10);
-
 }
