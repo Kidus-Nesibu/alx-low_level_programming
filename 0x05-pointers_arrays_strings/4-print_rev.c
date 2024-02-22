b638f461735abca99ef7399d40af1a72cc77239b
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-22 00:15:51 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,8 @@
 #include"main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: the argument that is given to accept string
+ */
 void print_rev(char *s)
 {
 	int i = 0;
@@ -7,6 +11,8 @@ void print_rev(char *s)
 	{
 		i++;
 	}
+
+	i = i -1;
 
 	while(i >= 0)
 	{
