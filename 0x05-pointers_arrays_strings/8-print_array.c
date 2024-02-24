5e152b7a195b98cac802a0fb11d9c9feaa00db91
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-23 16:54:44 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,10 @@
 #include"main.h"
 #include<stdio.h>
+/**
+ * print_array - prints elements of array that are specified
+ * @a: int pointer to accept the array
+ * @n: the number of array element to printed
+ */
 void print_array(int *a, int n)
 {
 	int i = 0;
