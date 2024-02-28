956b64eaf7c9060891bfccc91dfd6eb76305d3eb
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-28 08:17:37 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include"main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	for (; i<n; i++);
+	dest[i] = '\0';
+
+	return (dest);
+}
