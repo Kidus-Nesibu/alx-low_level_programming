590296a4b91401bf41d4fe01c7a55e5c3a260aae
Kidus-Nesibu <knesibu3@gmail.com>
2024-02-28 01:50:33 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * _strncat - concatnate string upto n
+ * @dest: string1
+ * @src: string2
+ * @n: number of string to be concatnated
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
