caad9794b9f183fc5357951aa5990fec77620973
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-01 07:46:48 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(char) * size);
+	ptr = (char *)malloc(sizeof(char) * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
