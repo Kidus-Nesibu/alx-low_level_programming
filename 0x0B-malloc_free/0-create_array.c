305130d2f8852a94f0e54aaa7d2632f4aa196614
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-01 07:58:13 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,11 @@
 #include"main.h"
 #include<stdlib.h>
+/**
+ * create_array - creates array by allocation memory form the heap
+ * @size: the size of the allocated memory from the heap
+ * @c: the charachter that initialze the char array
+ * Return: the allocated array ptr or NULL if size is 0 of memo alloc fail
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
