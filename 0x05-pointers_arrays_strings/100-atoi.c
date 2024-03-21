baf09e63ac673739b710655f435ab02001ca10a3
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-21 07:32:26 -0700

    dir

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include"main.h"
+int _atoi(char *s)
+{
+	int value 0;
+	int sign = 1;
+
+	while (*s = ' ')
+	{
+		s++;
+	}
+	
+	if (*s = '-')
+	{
+		sign = -1;
+	}
+
+	while (s[i] = '
+
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,8 +8,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-	for (; i<n; i++);
-	dest[i] = '\0';
+	while (i < n)
+	{
+		i++;
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
