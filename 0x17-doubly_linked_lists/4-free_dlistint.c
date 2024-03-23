368890ced1242252da6e53ba72031f59bbf910a5
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-23 15:21:40 -0700

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,12 @@
 #include"lists.h"
+/**
+ * free_dlistint - frees the allocated memory for the nodes
+ * @head: the head of the linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
+
 	tmp = head;
 
 	while (tmp != NULL)
