564476020c35efa59a98b720d1d9d00b882bc7d4
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-25 04:42:15 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-
-/**
- * insert_dnodeint_at_index - insert node at index
- * @h: linked list
- * @idx: index to be inserted at
- * @n: data
- *
- * Return: dlistint_t
- */
+#include"lists.h"
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *temp = *h;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+	dlistint_t *new;
 
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 
 	new->n = n;
-	if (idx == 0)
+
+	if(idx == 0)
 	{
 		new->next = *h;
 		new->prev = NULL;
 		if (*h != NULL)
-			(*h)->prev = new;
+		{
+			new->prev = *h;
+		}
 		*h = new;
 		return (new);
 	}
-	while (temp != NULL)
+
+	while (tmp != NULL)
 	{
 		if (i == idx)
 		{
-			new->next = temp;
-			temp->prev->next = new;
-			new->prev = temp->prev;
-			temp->prev = new;
+			new->next = tmp;
+			tmp->prev->next = new;
+			new->prev = tmp->prev;
+			tmp->prev = new;
+
 			return (new);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 		i++;
 	}
 	return (NULL);
