2564140421935f889301d6f6a4d9eeb05b1776f7
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-25 04:13:46 -0700

    task 7;

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->prev = NULL;
 		if (*h != NULL)
 		{
-			newprev>next = *h;
+			new->prev = *h;
 		}
 		*h = new;
 		return (new);
