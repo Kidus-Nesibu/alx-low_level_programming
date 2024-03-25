f1ca8b1282b3f3d5c757c23b9d83f3c482ca044d
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-25 04:45:50 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,54 @@
-#include"lists.h"
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at a given index of a
+ *                            doubly linked list.
+ * @head: Pointer to the head of the doubly linked list.
+ * @index: Index of the node to be deleted (starting from 0).
+ *
+ * Return: 1 if the deletion was successful, -1 if failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	dlistint_t *tmp = *h;
-	dlistint_t *new;
+    dlistint_t *current = *head;
+    dlistint_t *temp = NULL;
+    unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+    /* If the list is empty, return failure */
+    if (*head == NULL)
+        return -1;
 
-	new->n = n;
+    /* Special case: if the node to be deleted is the head node */
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return 1;
+    }
 
-	if(idx == 0)
-	{
-		new->next = *h;
-		new->prev = NULL;
-		if (*h != NULL)
-		{
-			new->prev = *h;
-		}
-		*h = new;
-		return (new);
-	}
+    /* Traverse the list to find the node at the given index */
+    while (current != NULL && i < index)
+    {
+        temp = current;
+        current = current->next;
+        i++;
+    }
 
-	while (tmp != NULL)
-	{
-		if (i == idx)
-		{
-			new->next = tmp;
-			tmp->prev->next = new;
-			new->prev = tmp->prev;
-			tmp->prev = new;
+    /* If the index is out of range, return failure */
+    if (current == NULL)
+        return -1;
 
-			return (new);
-		}
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-		i++;
-	}
-	return (NULL);
+    /* Update the links to bypass the node to be deleted */
+    temp->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = temp;
+
+    /* Free the memory allocated to the node */
+    free(current);
+
+    return 1;
 }
+
